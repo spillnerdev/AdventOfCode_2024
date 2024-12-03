99716ac1c54d2cc8808ff2eb07fded7eaab8f73e
Lukas Spillner <spillnerlu+github@gmail.com>
2024-12-03 08:08:16 +0100

    Added day-4 module.

---
+++ day-4/src/main/kotlin/DayFour.kt
@@ -0,0 +1,19 @@
+package dev.spillner
+
+import dev.spillner.aoc2024.Day
+import dev.spillner.aoc2024.interfaces.AoCTask
+
+
+object DayFour : AoCTask {
+    override val day: Day = Day.FOUR
+
+    override fun partOne(input: String) {
+    }
+
+    override fun partTwo(input: String) {
+    }
+}
+
+fun main() {
+    DayFour.run()
+}
